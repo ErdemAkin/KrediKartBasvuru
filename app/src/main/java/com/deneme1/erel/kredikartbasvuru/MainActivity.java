89bacf82cded332fc271de932537349c1d8665d0
Ali Erdem Akın <erdemakin09@gmail.com>
2016-12-05 21:04:25 +0300

    Initial commit

---
+++ app/src/main/java/com/deneme1/erel/kredikartbasvuru/MainActivity.java
@@ -0,0 +1,13 @@
+package com.deneme1.erel.kredikartbasvuru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//120202001 - Ali Erdem Akın
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
