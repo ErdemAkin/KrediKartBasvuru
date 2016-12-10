02d98adc3a197f8c4aa175cb3028dd8c08e0999d
Ali Erdem Akın <erdemakin09@gmail.com>
2016-12-10 13:36:36 +0300

    Arayüz Eklendi.

--- app/src/main/java/com/deneme1/erel/kredikartbasvuru/MainActivity.java
+++ app/src/main/java/com/deneme1/erel/kredikartbasvuru/MainActivity.java
@@ -2,6 +2,9 @@ package com.deneme1.erel.kredikartbasvuru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 //120202001 - Ali Erdem Akın
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText et = (EditText) findViewById(R.id.editText);
+        final EditText et2 = (EditText) findViewById(R.id.editText2);
+        final EditText et3 = (EditText) findViewById(R.id.editText3);
+        final EditText et4 = (EditText) findViewById(R.id.editText4);
+        final EditText et5 = (EditText) findViewById(R.id.editText5);
+        final EditText et6 = (EditText) findViewById(R.id.editText6);
+        final EditText et7 = (EditText) findViewById(R.id.editText7);
+        final EditText et8 = (EditText) findViewById(R.id.editText8);
+        final EditText et9 = (EditText) findViewById(R.id.editText9);
+        final EditText et10 = (EditText) findViewById(R.id.editText10);
+        final EditText et11 = (EditText) findViewById(R.id.editText11);
+        final EditText et12 = (EditText) findViewById(R.id.editText12);
+
+
+        Button buton = (Button) findViewById(R.id.button);
+
     }
 }
