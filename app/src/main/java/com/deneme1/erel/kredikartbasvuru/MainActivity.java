6ede1dc2574d23ac399c9bc769694ae081844fc3
bsrnr <130202027@kocaeli.edu.tr>
2016-12-12 01:30:45 +0200

    Scroll View CheckBox Radio Button vb. View ların kullanılması

--- app/src/main/java/com/deneme1/erel/kredikartbasvuru/MainActivity.java
+++ app/src/main/java/com/deneme1/erel/kredikartbasvuru/MainActivity.java
@@ -2,8 +2,13 @@ package com.deneme1.erel.kredikartbasvuru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 //120202001 - Ali Erdem Akın
 
@@ -19,26 +24,63 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup rg;
+    RadioButton rb1, rb2;
+    private CheckBox cb1, cb2, cb3, cb4, cb5;
+    private Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText et = (EditText) findViewById(R.id.editText);
-        final EditText et2 = (EditText) findViewById(R.id.editText2);
-        final EditText et3 = (EditText) findViewById(R.id.editText3);
-        final EditText et4 = (EditText) findViewById(R.id.editText4);
-        final EditText et5 = (EditText) findViewById(R.id.editText5);
-        final EditText et6 = (EditText) findViewById(R.id.editText6);
-        final EditText et7 = (EditText) findViewById(R.id.editText7);
-        final EditText et8 = (EditText) findViewById(R.id.editText8);
-        final EditText et9 = (EditText) findViewById(R.id.editText9);
-        final EditText et10 = (EditText) findViewById(R.id.editText10);
-        final EditText et11 = (EditText) findViewById(R.id.editText11);
-        final EditText et12 = (EditText) findViewById(R.id.editText12);
+        rg = (RadioGroup) findViewById(R.id.rgroup);
+        rb1 = (RadioButton) findViewById(R.id.rbutton1);
+        rb2 = (RadioButton) findViewById(R.id.rbutton2);
 
+        cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        cb4 = (CheckBox) findViewById(R.id.checkBox4);
+        cb5 = (CheckBox) findViewById(R.id.checkBox5);
+        b1 = (Button) findViewById(R.id.button1);
 
-        Button buton = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                int butonid = rg.getCheckedRadioButtonId();
+                String text = "";
+                switch (butonid) {
+                    case R.id.rbutton1: {
+                        text = "BAYAN";
+                        break;
+                    }
+                    case R.id.rbutton2: {
+                        text = "BAY ";
+                        break;
+                    }
+                }
+
+                if (cb1.isChecked()) {
+                    text = text + " " + cb1.getText().toString() + "\n";
+                }
+                if (cb2.isChecked()) {
+                    text = text + " " + cb2.getText().toString() + "\n";
+                }
+                if (cb3.isChecked()) {
+                    text = text + " " + cb3.getText().toString() + "\n";
+                }
+                if (cb4.isChecked()) {
+                    text = text + " " + cb4.getText().toString() + "\n";
+                }
+                if (cb5.isChecked()) {
+                    text = text + " " + cb5.getText().toString() + "\n";
+                }
+
+                Toast.makeText(getApplicationContext(), "Yanıtlarınız gönderildi: \n" + text, 0).show();
+
+            }
+        });
 
     }
 }
